f68cf4c414fa04ab7f4c4fd75e002ad53ce3db18
aMinjon1065 <59558450+aminjon1065@users.noreply.github.com>
2023-04-06 02:37:37 +0500

    0.0.1

--- src/components/UI/Drawer/Bell/index.jsx
+++ src/components/UI/Drawer/Bell/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import {View} from "react-native";
+import { View } from "react-native";
 import BellIcon from 'react-native-vector-icons/Feather'
-
+import { Badge } from 'react-native-paper';
 const Index = () => {
     return (
         <View className={"pr-2"}>
-            <BellIcon name={"bell"} size={28} color={"#146C94"}/>
+            <View className="flex flex-row">
+                <Badge className="w-0 bottom-3" size={10} />
+                <BellIcon name={"bell"} size={28} color={"#146C94"} />
+            </View>
         </View>
     );
 };
--- src/screens/Main/Home/index.jsx
+++ src/screens/Main/Home/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image, TouchableHighlight } from "react-native";
+import { View, Text, Image, TouchableWithoutFeedback, ImageBackground } from "react-native";
 import { LinearGradient } from 'expo-linear-gradient';
 import FirstHelpIcon from './../../../assets/img/Icons/Frame.png';
 import WhatToDoIcon from './../../../assets/img/Icons/icons8-inquiry-100.png';
@@ -10,8 +10,12 @@ import CallIcon from 'react-native-vector-icons/MaterialIcons';
 import MessageIcon from 'react-native-vector-icons/MaterialIcons';
 import { TextInput, Searchbar } from 'react-native-paper';
 import { ScrollView } from 'react-native-gesture-handler';
-
+import MapImg from './../../../assets/img/map.jpg';
 const Index = () => {
+    const ref = React.useRef(null);
+    const [searchQuery, setSearchQuery] = React.useState('');
+    const onChangeSearch = query => setSearchQuery(query);
+
     return (
         <ScrollView>
             <View
@@ -21,57 +25,57 @@ const Index = () => {
                     <View className={""}>
                         <Searchbar
                             className={"bg-slate-100 mb-5 rounded text-slate-300"}
-                            clearTextOnFocus={true}
-                            clearIcon={"close"}
-                            clearButtonMode="always"
-                            // outlineColor={"transparent"}
-                            // right={
-                            //     <TextInput.Icon icon={() => <SearchIcon name={"search"} size={32}/>}/>
-                            // }
-                            // underlineStyle={{borderStyle:"dotted"}}
+                            onIconPress={() => ref.current.focus()}
+                            ref={ref}
+                            placeholder="Поиск"
+                            placeholderTextColor='#E0E0E0'
+                            onChangeText={onChangeSearch}
+                            value={searchQuery}
                             iconColor={'#146C94'}
                         />
                     </View>
                     <LinearGradient
                         start={[0.5, 1]} end={[0, 0.1]}
                         // Background Linear Gradient
-                        className={"rounded-lg shadow shadow-indigo-800 bg-pink-500 mb-2 mt-5"}
+                        className={"rounded-lg shadow shadow-indigo-800 bg-pink-500  mt-5"}
                         colors={['#e18d98', '#d57a80', 'transparent']}
                     // colors={['#e18d98', '#d57a80', 'transparent']}
                     >
-                        <View className={"flex flex-row justify-around rounded h-24"}>
-                            <View className={""}>
-                                <Image source={FirstHelpIcon} className={"object-top bottom-5"} />
+                        <TouchableWithoutFeedback onPress={() => console.log("First Help")}>
+                            <View className={"flex flex-row justify-around rounded h-24"}>
+                                <View className={""}>
+                                    <Image source={FirstHelpIcon} className={"object-top bottom-5"} />
+                                </View>
+                                <View className={"flex-1 justify-center "}>
+                                    <Text className={"text-2xl text-center text-white"}>
+                                        Помощь
+                                    </Text>
+                                    <Text className={"text-center text-white"}>
+                                        Первая помощь пострадавшему
+                                    </Text>
+                                </View>
                             </View>
-                            <View className={"flex-1 justify-center "}>
-                                <Text className={"text-2xl text-center text-white"}>
-                                    Помощь
-                                </Text>
-                                <Text className={"text-center text-white"}>
-                                    Первая помощь пострадавшему
-                                </Text>
-                            </View>
-                        </View>
+                        </TouchableWithoutFeedback>
                     </LinearGradient>
                     <View className=" flex flex-row justify-between">
                         <LinearGradient
                             start={[0, 0]}
                             end={[1, 1]}
                             // Background Linear Gradient
-                            className={"rounded-lg shadow shadow-indigo-800 bg-slate-600 mb-2 mt-5 w-6/12 mr-0.5 pb-1"}
+                            className={"rounded-lg  shadow-indigo-800 bg-slate-600 mb-2 mt-5 w-6/12 mr-0.5 pb-1"}
                             colors={['#8BC6EC', '#9599E2', 'transparent']}
                             location={[0.25, 0.4, 1]}
                         // colors={['#e18d98', '#d57a80', 'transparent']}
                         >
                             <View className={"items-center"}>
-                                <Image source={WhatToDoIcon} className={"object-top bottom-5"} />
+                                <Image source={WhatToDoIcon} className={"object-top "} />
                             </View>
                             <View className={"justify-center "}>
                                 <Text className={"text-2xl text-center text-white"}>
                                     Что делать?
                                 </Text>
                                 <Text className={"text-center text-white"}>
-                                    Что делать в экстренных ситуаций
+                                    Что делать в экстренных ситуациях
                                 </Text>
                             </View>
                         </LinearGradient>
@@ -80,12 +84,12 @@ const Index = () => {
                             end={[1, 1]}
                             location={[0.25, 0.4, 1]}
                             // Background Linear Gradient
-                            className={"rounded-lg shadow shadow-indigo-800 bg-indigo-400 mb-2 mt-5 w-6/12  pb-1"}
+                            className={"rounded-lg shadow-indigo-800 bg-indigo-400 mb-2 mt-5 w-6/12  pb-1"}
                             colors={['#74EBD5', '#9FACE6', 'transparent']}
                         // colors={['#e18d98', '#d57a80', 'transparent']}
                         >
                             <View className={"items-center"}>
-                                <Image source={SaveKidsIcon} className={"object-top bottom-5"} />
+                                <Image source={SaveKidsIcon} className={"object-top"} />
                             </View>
                             <View className={" justify-center "}>
                                 <Text className={"text-2xl text-center text-white"}>
@@ -97,28 +101,37 @@ const Index = () => {
                             </View>
                         </LinearGradient>
                     </View>
-                </View>
-                <View className={"p-2 mt-4"}>
-                    <Text className="text-center text-xl text-slate-800">
-                        Нужна помощь специалиста КЧС?
-                    </Text>
-                    <View className="flex flex-row pt-2">
-                        <TouchableHighlight className="flex-1 mr-1">
-                            <View className="flex flex-row items-center justify-center rounded py-3 bg-red-500">
-                                <CallIcon name='phone' size={32} color={"white"} />
-                                <Text className="text-center text-white ml-3">
-                                    Позвонить
+                    <View className={"mt-4"}>
+                        <Text className="text-center text-xl text-slate-800">
+                            Нужна помощь специалиста КЧС?
+                        </Text>
+                        <View className="flex flex-row pt-2">
+                            <TouchableWithoutFeedback onPress={() => console.log("call")}>
+                                <View className="flex flex-row flex-1 mr-1 items-center justify-center rounded-xl py-3 bg-red-500">
+                                    <CallIcon name='phone' size={32} color={"white"} />
+                                    <Text className="text-center text-white ml-3">
+                                        Позвонить
+                                    </Text>
+                                </View>
+                            </TouchableWithoutFeedback>
+                            <TouchableWithoutFeedback onPress={() => console.log("message")}>
+                                <View className="flex flex-row flex-1 ml-1 items-center justify-center rounded-xl py-3 bg-cyan-800">
+                                    <MessageIcon name='message' size={32} color={"white"} />
+                                    <Text className="text-center text-white ml-3">
+                                        Написать
+                                    </Text>
+                                </View>
+                            </TouchableWithoutFeedback>
+                        </View>
+                    </View>
+                    <View className="bg-slate-50 mt-4 rounded-2xl">
+                        <TouchableWithoutFeedback onPress={() => console.log("map")}>
+                            <ImageBackground source={MapImg} blurRadius={5} imageStyle={{ borderRadius: 10 }} className="  h-32 flex flex-row items-center justify-center">
+                                <Text className="text-center text-xl">
+                                    Map
                                 </Text>
-                            </View>
-                        </TouchableHighlight>
-                        <TouchableHighlight className="flex-1 ml-1">
-                            <View className="flex flex-row items-center justify-center rounded py-3 bg-cyan-800">
-                                <MessageIcon name='message' size={32} color={"white"} />
-                                <Text className="text-center text-white ml-3">
-                                    Написать
-                                </Text>
-                            </View>
-                        </TouchableHighlight>
+                            </ImageBackground>
+                        </TouchableWithoutFeedback>
                     </View>
                 </View>
                 <StatusBar style={"auto"} />
