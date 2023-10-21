2b2d4917720514c3de566e48fee12be295ef1d98
aMinjon1065 <59558450+aminjon1065@users.noreply.github.com>
2023-10-21 20:02:50 +0500

    0.0.1

---
+++ src/screens/Main/Quiz/ru/index.jsx
@@ -0,0 +1,98 @@
+import React, {useState} from 'react';
+import {Text, TouchableOpacity, View} from "react-native";
+import {getRandomQuestions} from "../../../../utils/quizUtils";
+import {questionsRu} from "../../../../data/QuizRu";
+import {Button} from "react-native-paper";
+import AnimatedLottieView from "lottie-react-native";
+import animate from '../../../../assets/animation_lnobi847.json';
+import animate2 from '../../../../assets/animation_lnobhxex.json';
+import {useSelector} from "react-redux";
+
+const Index = () => {
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [score, setScore] = useState(0);
+    const [quizCompleted, setQuizCompleted] = useState(false);
+    const [questions, setQuestions] = useState(getRandomQuestions(questionsRu, 10));
+    const handleAnswer = (selectedAnswer) => {
+        if (quizCompleted) {
+            return;
+        }
+        const currentQuestionData = questions[currentQuestion];
+        const correctAnswer = currentQuestionData.correct; // Правильный ответ
+        if (correctAnswer === selectedAnswer) {
+            setScore(score + 1);
+        }
+        if (currentQuestion + 1 < questions.length) {
+            setCurrentQuestion(currentQuestion + 1);
+        } else {
+            setQuizCompleted(true);
+        }
+    };
+    const restartQuiz = () => {
+        setQuestions(getRandomQuestions(questionsRu, 10));
+        setCurrentQuestion(0);
+        setScore(0);
+        setQuizCompleted(false);
+    };
+    const darkModeSelector = useSelector(state => state.theme.darkMode);
+    return (
+        quizCompleted ?
+            <View className={`h-screen w-screen ${darkModeSelector ? "bg-slate-950" : "bg-white"}`}>
+                <View className={`mx-auto ${darkModeSelector ? "bg-slate-950" : "bg-white"}`}>
+                    {score < 5 ?
+                        <AnimatedLottieView source={animate2} className={"w-11/12 h-auto flex flex-row"} autoPlay={true}
+                                            loop={true}/>
+                        :
+                        <AnimatedLottieView className={"w-11/12 h-auto flex flex-row"} autoPlay={true} loop={true}
+                                            source={animate}/>
+                    }
+                    <View className={"flex-1"}>
+                        <View className={"items-center justify-center my-auto mx-auto"}>
+                            <Text
+                                className={`text-center text-xl ${darkModeSelector ? "text-white" : "text-slate-950"}`}>Опрос
+                                завершен</Text>
+                            <Text className={darkModeSelector ? "text-white" : "text-slate-950"}>Ваш
+                                счет: {score} из {questions.length}
+                            </Text>
+                        </View>
+                    </View>
+                    <View className={"mb-2"}>
+                        <View className={"items-center justify-center my-auto mx-auto"}>
+                            <Text
+                                className={`text-center text-xl ${darkModeSelector ? "text-white" : "text-slate-950"}`}>Опрос
+                                завершен</Text>
+                            <Text className={darkModeSelector ? "text-white" : "text-slate-950"}>
+                                Вы правильно ответили: {score} из {questions.length} вопросов
+                            </Text>
+                        </View>
+                    </View>
+                    <View className={"mx-auto"}>
+                        <Button onPress={() => restartQuiz()} className={"w-5/12 bg-slate-800 rounded"}><Text
+                            className={"text-white"}>New</Text></Button>
+                    </View>
+                </View>
+            </View>
+            :
+            <View className={`h-screen w-screen ${darkModeSelector ? "bg-slate-950" : "bg-white"}`}>
+                <Text className={"text-base text-center"}>{currentQuestion} из {questions.length} вопросов</Text>
+                <View className={`flex-1`}>
+                    <Text
+                        className={`flex-wrap text-2xl text-center ${darkModeSelector ? "text-white" : "text-slate-950"}`}>{questions[currentQuestion].question}</Text>
+                    {questions[currentQuestion].answers.map((answer, index) => (
+                        <TouchableOpacity
+                            key={index}
+                            onPress={() => handleAnswer(answer)}
+                            className={"w-11/12 mx-auto my-2 bg-slate-800 rounded flex-wrap"}
+                        >
+                            <View className={"px-2 py-4"}>
+                                <Text className={"text-white"}>
+                                    {answer}
+                                </Text>
+                            </View>
+                        </TouchableOpacity>
+                    ))}
+                </View>
+            </View>
+    );
+};
+export default Index;
--- src/screens/Main/Weathers/index.jsx
+++ src/screens/Main/Weathers/index.jsx
@@ -2,15 +2,51 @@ import React from 'react';
 import {View, Text} from "react-native";
 import NavigationBackBar from "../../../components/UI/NavigationBackBar";
 import {useSelector} from "react-redux";
+import {useTranslation} from "react-i18next";
+import WebView from "react-native-webview";
 
 const Index = (props) => {
     const selectLocation = useSelector(state => state.locationState);
+    const {i18n} = useTranslation();
+    const darkModeSelector = useSelector(state => state.theme.darkMode);
+    const htmlContent = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>IFrame Example</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+          body {
+            margin: 0;
+            padding: 0;
+          }
+           iframe {
+    width: 100vw;
+    height: 300vh;
+    border: none;
+    overflow: hidden;    
+  }
+        iframe {
+        transform: translateY(-22%);
+        }
+        </style>
+      </head>
+      <body>
+        <iframe src="https://www.meteo.tj/${i18n.language}/weather-forecast"></iframe>
+      </body>
+    </html>
+  `;
+
+
+    // https://www.meteo.tj/${i18n.language}/weather-forecast
     return (
         <>
             <NavigationBackBar props={props}/>
-            <View>
-                <Text>{selectLocation.latitude} - {selectLocation.longitude}</Text>
-            </View>
+            <WebView
+                originWhitelist={['*']}
+                className={" h-screen w-full"}
+                source={{html: htmlContent}}
+            />
         </>
     );
 };
